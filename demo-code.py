83fda91930716db92ebdfa69bdf466a9fb5a9397
Raghu <raghu.esh@gmail.com>
2025-02-02 12:57:42 +0530

    Create demo-code.py

---
+++ demo-code.py
@@ -0,0 +1,10 @@
+from demo_math_ops import add, sub, mult, div
+
+# Initialise some integer variables
+value1 = 5
+value2 = 3
+
+print("Adding {0} and {1} gives {2}".format(value1, value2, add(value1, value2)))
+print("Subtracting {0} from {1} results in {2}" .format(value1,value2, sub(value1,value2)))
+print("Multiplying {0} by {1} equals {2}" .format(value1,value2, mult(value1,value2)))
+print("Dividing {0} by {1} gives {2}" .format(value1,value2, div(value1,value2)))
